fb88e551663d1127683d7a5b4e94d5ec524f8548
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-05 16:30:32 -0400

    Complete JS Exercise10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if it's divisible by 400 return true
+
+    if (year % 400 == 0){
+        return true;
+    }
+
+    //if its divisible by 100 (and not 400 in this case) return false
+    if (year % 100 == 0){
+        return false;
+    }
+    //if it's divisibly by 4 (and not 100) return true
+    if (year % 4 == 0){
+        return true;
+    }
+
+
+    return false;
 
 };
 
