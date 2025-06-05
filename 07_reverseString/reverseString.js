33faa008edd0e3f3061f43daec3ae9fc5df56d02
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-05 15:31:11 -0400

    Complete JS Exercise7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    reversed = "";
+
+    for (let i = string.length -1; i >= 0; i--){
+        reversed += string[i];
+    };
+
+
+    return reversed;
 
 };
 
