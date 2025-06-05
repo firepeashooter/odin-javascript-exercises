5bbfedb1fc49a718130f13298fc621b6e3e7aeef
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-05 15:19:40 -0400

    Complete JS Exercise6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
 
+    repeatedString = "";
+
+    for (let i = 0; i < num; i++){
+        repeatedString += string;
+    }
+
+
+    return repeatedString;
 };
+
+const string = "hey";
+const num = 3;
+
+console.log(repeatString(string, num));
 
 // Do not edit below this line
 module.exports = repeatString;
