a380cfa9ab48812e25eee1113b4d82a56177c81b
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-05 16:05:54 -0400

    Complete JS Exersice8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    newArray = []
+
+    for (let i = 0; i < array.length; i++){
+
+        
+        if (!(args.includes(array[i]))){
+            newArray.push(array[i])
+        }
+    }
+
+    return newArray;
+        
 };
 
 // Do not edit below this line
