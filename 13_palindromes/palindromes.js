bb74649c266296f49f80a6a27ab6bd4a117f8905
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-11 23:18:37 -0400

    Complete JS Exercise13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+const palindromes = function (input) {
+
+    //Makes the input lowercase
+    input = input.toLowerCase();    
+
+    backwardsString = "";
+    formattedString = "";
+
+    //Format the input to get rid of any punctuation
+
+    for (let i = 0; i < input.length; i++){
+        if ("abcdefghijklmnopqrstuvwxyz123456789".includes(input[i])){
+            formattedString += input[i];
+        }
+    }
+
+    console.log(formattedString);
+
+    //loop through the string backwards and add it to a new string
+
+    for (let i = formattedString.length - 1; i >= 0; i --){
+        backwardsString += formattedString[i];
+    }
+
+    console.log(backwardsString);
+
+    //return the comparison of the two strings
+
+    return formattedString == backwardsString;
 
 };
 
